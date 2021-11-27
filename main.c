90359be6984a5f45f9d31abc7d1974babd2247a1
Mikhail Shcherbakov <yuske.dev@gmail.com>
2021-11-27 18:53:04 +0100

    Dummy impl

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+
+#ifdef _WIN32
+#define PATH_JOIN_SEPERATOR   "\\"
+#else
+#define PATH_JOIN_SEPERATOR   "/"
+#endif
+
+int main(int argc, char const *argv[]) {
+
+  time_t t;
+  time(&t);
+  struct tm tm = *localtime(&t);
+
+  char filename[256];
+  sprintf_s(filename, 256, "_pwned-%d-%d-%d--%d-%d-%d--%d", 
+    tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900, tm.tm_hour, tm.tm_min, tm.tm_sec, t);
+
+  FILE *fp = fopen(filename, "w");
+  if (fp == NULL)
+  {
+      printf("Error opening the file %s", filename);
+      return -1;
+  }
+
+  for (size_t i = 0; i < argc; i++)
+  {
+    fprintf(fp, "%s ", argv[i]);
+  }
+  
+  fclose(fp);
+  return 0;
+}
